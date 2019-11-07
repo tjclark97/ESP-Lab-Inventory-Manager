40c42270d4318d8dc7b52368b8e5890701a082ec
Ervin Lopez <ervinlopez1000@gmail.com>
2019-11-07 12:11:59 -0600

    Implemented Iteration 3 chores and user stories

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ActiveRecord::Base
 	@all_categories = {}
 	@item_list = []
+	@all_items
 
 	def self.get_updated_categories
 		@all_categories
@@ -11,11 +12,26 @@ class Item < ActiveRecord::Base
 	end
 
 	def self.with_categories(categories)
-		puts "From Model categories = ", categories
-		self.where({category: categories})
+		all_items = self.where({category: categories}).uniq
+
+		previous_names = []
+		index_list = []
+		all_items.each do |item|
+			if !(previous_names.include? item.name)
+				index_list << item
+			end
+
+			previous_names << item.name
+		end
+
+		index_list
 	end
 
 	def self.specific_item_list(specific_item_name)
 		@item_list = self.where({name: specific_item_name})
+	end
+
+	def self.get_item_quantity(specific_item_name)
+		self.where({name: specific_item_name}).count
 	end
 end
