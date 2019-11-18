8358757f05f5071f9a1677b0f92f9c374ea68e34
Ervin Lopez <ervinlopez1000@gmail.com>
2019-11-18 14:47:23 -0600

    Changed seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-items = [{:category => 'Microscopes', :name => 'AmScope B490 Compound Microscope', :condition => '10', :available => '1', :description => 'Device to view small structures', :purchase_link => 'google.com'},
-         {:category => 'Microscopes', :name => 'AmScope B490 Compound Microscope', :condition => '9', :available => '0', :description => 'Device to view small structures', :purchase_link => 'facebook.com'},
-         {:category => 'Microscopes', :name => 'AmScope B490 Compound Microscope', :condition => '8', :available => '1', :description => 'Device to view small structures', :purchase_link => 'amazon.com'}
+items = [{:category => 'Microscopes', :name => 'AmScope B490 Compound Microscope', :quantity => '3', :condition => '10', :available => '1', :description => 'Device to view small structures', :purchase_link => 'google.com'},
+         {:category => 'Microscopes', :name => 'AmScope B490 Compound Microscope', :quantity => '3', :condition => '9', :available => '0', :description => 'Device to view small structures', :purchase_link => 'facebook.com'},
+         {:category => 'Microscopes', :name => 'AmScope B490 Compound Microscope', :quantity => '3', :condition => '8', :available => '1', :description => 'Device to view small structures', :purchase_link => 'amazon.com'}
   	 ]
 
 items.each do |item|
