691cf6245727fe8ddb2fd210d84a960c1655895e
Ervin Lopez <ervinlopez1000@gmail.com>
2019-11-18 09:46:09 -0600

    Fixed mispelling

--- db/migrate/20191016142446_create_items.rb
+++ db/migrate/20191016142446_create_items.rb
@@ -3,6 +3,7 @@ class CreateItems < ActiveRecord::Migration
     create_table :items do |t|
       t.string :category
       t.string :name
+      t.integer :quantity
       t.integer :condition
       t.integer :available
       t.string :description
