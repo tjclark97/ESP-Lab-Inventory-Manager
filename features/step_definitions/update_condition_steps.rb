b07732b6dfee6b7e8295f6057251ca3d39e84141
huffs0915 <43215270+huffs0915@users.noreply.github.com>
2019-12-02 14:49:34 -0600

    Create update_condition_steps.rb

---
+++ features/step_definitions/update_condition_steps.rb
@@ -0,0 +1,11 @@
+Given("I am at the the show_specific_item1 page") do
+  visit '/'
+end
+
+When("I press {string}") do |string|
+  visit '/'
+end
+
+Then("I should see the item {string}") do |string|
+  visit '/'
+end
