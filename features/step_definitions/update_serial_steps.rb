e0d43396711037dad287e9623d3eb9ffa67bf3c9
huffs0915 <43215270+huffs0915@users.noreply.github.com>
2019-12-02 14:50:01 -0600

    Create update_serial_steps.rb

---
+++ features/step_definitions/update_serial_steps.rb
@@ -0,0 +1,11 @@
+Given("I am at the the show_specific_item2 page") do
+  visit '/'
+end
+
+When("I press {string}") do |string|
+  visit '/'
+end
+
+Then("I should see that the item {string}") do |string|
+  visit '/'
+end
